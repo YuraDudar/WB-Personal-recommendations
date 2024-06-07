843a23d147f5c3a540dbf0b90a2a3f88dfc136af
Aleksey Fedorov <hedgefo9@hedgefo9-2.local>
2024-06-07 18:35:56 +0300

    Реализовал сервис взаимодействия с базой данных

---
+++ backend/db-service/src/test/java/ru/sberpo300/WBrecommendation/db_service/DbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sberpo300.WBrecommendation.db_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
